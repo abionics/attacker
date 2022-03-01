49c317b10eeadf3f22181c9f3f69801fdb54a8e1
sergisoft <100694386+tsergiosoft@users.noreply.github.com>
2022-03-01 18:44:03 +0200

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,1 +1,12 @@
+#bash
+docker-compose down
+docker rm -f $(docker ps --format "{{.ID}}")
+docker image prune --all
 
+git pull origin master
+
+count=$1
+if [ -z "$count" ]; then
+  count=10
+fi
+docker-compose up --build -d --scale app=$count
