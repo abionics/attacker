c36171827d8f64d77129de9f54e6f5e38baf6e6f
sergisoft <100694386+tsergiosoft@users.noreply.github.com>
2022-03-02 14:24:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,24 @@
 #bash
-docker-compose down
-docker rm -f $(docker ps --format "{{.ID}}")
-docker image prune --all
+#docker-compose down
+#docker rm -f $(docker ps --format "{{.ID}}")
+#docker image prune --all
+#git pull origin master
 
-git pull origin master
+echo "Starting DRipper containers..."
 
-count=$1
-if [ -z "$count" ]; then
-  count=10
-fi
-docker-compose up --build -d --scale app=$count
+for i in {1..3}; do
+  count=$1
+  if [ -z "$count" ]; then
+   count=10
+  fi
+  docker-compose up --build -d --scale app=$count
+  
+  echo "Sleep 30..."
+  
+  sleep 30 # run time
+   
+  echo "down..."
+  docker-compose down
+   
+   sleep 5
+done
