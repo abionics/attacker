b424775a537a282d29eda2c40ef6ffb8b6033bf3
sergisoft <100694386+tsergiosoft@users.noreply.github.com>
2022-03-02 14:30:23 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
-#bash
-count=$1
-if [ -z "$count" ]; then
-  count=10
-fi
-docker-compose up --build -d --scale app=$count
+echo "Starting DRipper containers..."
+
+for i in {1..3}; do
+  count=$1
+  if [ -z "$count" ]; then
+   count=2
+  fi
+  docker-compose up --build -d --scale app=$count
+
+  echo "Sleep 30..."
+
+  sleep 30 # run time
+
+  echo "down..."
+  docker-compose down
+
+   sleep 5
+done
