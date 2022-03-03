1fd4baa36e9b1096999fd0620014654eff721d78
sergisoft <100694386+tsergiosoft@users.noreply.github.com>
2022-03-03 11:30:12 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,28 @@
 #bash
-count=$1
-if [ -z "$count" ]; then
-  count=10
-fi
-docker-compose up --build -d --scale app=$count
+#cd attacker
+
+for i in {1..3}; do
+
+ echo "Starting DRipper containers..."
+
+ docker-compose down
+ docker rm -f $(docker ps --format "{{.ID}}")
+ docker image prune --all --force
+ git init .
+ git pull origin master
+
+  count=$1
+  if [ -z "$count" ]; then
+   count=1
+  fi
+  docker-compose up --build -d --scale app=$count
+
+  echo "Sleep 30..."
+
+  sleep 30 # run time
+
+  echo "down..."
+  docker-compose down
+
+  sleep 5
+done
