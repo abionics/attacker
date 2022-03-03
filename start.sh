8826b759c2c71d3a8a9559e3eeca628dde42a046
sergisoft <100694386+tsergiosoft@users.noreply.github.com>
2022-03-03 15:45:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #bash
 docker-compose down
-docker rm -f $(docker ps --format "{{.ID}}")
+#docker rm -f $(docker ps --format "{{.ID}}")
 #git init .
 #docker image prune --all
 git pull origin master
 
 count=$1
 if [ -z "$count" ]; then
-  count=10
+  count=1
 fi
-docker-compose up --build -d --scale app=$count
+# docker-compose up --build -d --scale app=$count
